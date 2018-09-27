ba9de8edc04e62febb297d6c5907220022afb129
Jakub Pavlík <severus@post.cz>
2018-09-27 17:09:23 +0200

    BR: generate annotations for composed/modified chants

---
+++ breviarium_romanum/scripts/annotationes.rb
@@ -0,0 +1,33 @@
+require 'gly'
+
+parser = Gly::Parser.new
+
+documents = ARGV.collect do |path|
+  parser.parse path
+end
+documents.sort_by! do |document|
+  # sort by date
+  document.header['dies'].split('/').collect(&:to_i)
+end
+
+File.open('annotationes.tex', 'w') do |fw|
+  documents.each do |document|
+    with_annotations = document.scores.select {|score| score.headers.has_key? 'ratio' }
+    next if with_annotations.empty?
+
+    fw.puts "\\textbf{#{document.header['festum']}}"
+    fw.puts
+
+    with_annotations.each do |score|
+      lyrics = score
+               .lyrics
+               .readable
+               .gsub(/\s+\*\s+/, ' ')
+               .gsub(/e u o u a e\.?/i, '')
+               .gsub(/\.\s*$/, '')
+      fw.puts "\\emph{#{lyrics}}:"
+      fw.puts score.headers['ratio']
+      fw.puts
+    end
+  end
+end
